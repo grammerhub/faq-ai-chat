1962f7ced971893c9fc86f358db420cfd3ddbb2e
andrea-everett <andrea.dawn.everett@gmail.com>
2023-02-08 19:35:42 -0500

    add files

--- beam-app/app.py
+++ beam-app/app.py
@@ -1,69 +1,24 @@
-import requests
-from bs4 import BeautifulSoup
+"""
+This example is a conversational AI app, using LangChain.
+Users can ask queries about the day's New York Times headlines, and the conversational AI will answer questions about them.
+It's inspired by this post:
+https://langchain.readthedocs.io/en/latest/modules/chains/combine_docs_examples/question_answering.html
+"""
+import beam
 
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.faiss import FAISS
-from langchain.docstore.document import Document
-from langchain.prompts import PromptTemplate
+app = beam.App(
+    name="conversational-ai",
+    cpu=8,
+    gpu=1,
+    memory="32Gi",
+    python_packages=["bs4", "openai", "langchain", "faiss-cpu"],
+)
 
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
+# Triggers determine how your app is deployed
+app.Trigger.RestAPI(
+    inputs={"query": beam.Types.String()},
+    outputs={"pred": beam.Types.Json()},
+    handler="run.py:start_conversation",
+)
 
-# We'll save our headlines to this path
-file_path = "/workspace/transcript.txt"
-
-
-# Download headlines from NYT
-def download_headlines():
-    res = requests.get("https://www.grammerhub.org")
-    soup = BeautifulSoup(res.content, "html.parser")
-    # Grab all headlines
-    headlines = soup.select_one(".content")
-    parsed_headlines = []
-    for h in headlines:
-        parsed_headlines.append(h.get_text())
-
-    print(parsed_headlines[0])
-
-    # Write headlines to a text file
-    with open(file_path, "w") as f:
-        f.write(str(parsed_headlines))
-        f.close()
-
-
-# Answer questions about the headlines
-def start_conversation(**inputs):
-    # Grab the input from the API
-    query = inputs["query"]
-    # Download headlines from nytimes.com and save to the file path above
-    download_headlines()
-
-    with open(file_path) as f:
-        saved_file = f.read()
-        # Split the text to conform to maximum number of tokens
-        text_splitter = CharacterTextSplitter(
-            separator="\n\n",
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len,
-        )
-
-        texts = text_splitter.split_text(saved_file)
-        embeddings = OpenAIEmbeddings()
-        docsearch = FAISS.from_texts(texts, embeddings)
-        docs = docsearch.similarity_search(query)
-
-        chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff")
-        res = chain(
-            {"input_documents": docs, "question": query}, return_only_outputs=True
-        )
-        print(res)
-        return {"pred": res}
-
-
-if __name__ == "__main__":
-    # # You can customize this query however you want:
-    # # For example: What happened in Washington today?
-    query = "When do they meet"
-(start_conversation(query=query))
+app.Output.File(name="transcript", path="transcript.txt")
