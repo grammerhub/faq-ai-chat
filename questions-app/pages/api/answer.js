c67f6175c184143cae0b6ce63822a846fafed819
andrea-everett <andrea.dawn.everett@gmail.com>
2023-02-10 13:09:56 -0500

    change autho

--- questions-app/pages/api/answer.js
+++ questions-app/pages/api/answer.js
@@ -2,7 +2,7 @@
 
 // Creates a fetch request to beam api and returns answer back based on query given
 export default async function handler(req, res) {
-  const response = await fetch('https://beam.slai.io/b7dpa', {
+  const response = await fetch('https://beam.slai.io/l9hn6', {
     method: 'POST',
     headers: {
       // authorization value to gain access to beam api end point
