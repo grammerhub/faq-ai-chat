7b1674693d443775814b4fa855bd053d9b0dffc5
ZephyrPfeiffer <zephyrpfeiffer@gmail.com>
2023-02-28 14:14:46 -0500

    Merge branch 'dev' of https://github.com/grammerhub/faq-ai-chat into dev

--- questions-app/pages/api/answer.js
+++ questions-app/pages/api/answer.js
@@ -1,25 +1,32 @@
 /// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import * as cheerio from 'cheerio'
+import axios from 'axios'
+
 // Creates a fetch request to beam api and returns answer back based on query given
 export default async function handler(req, res) {
-  const response = await fetch('https://beam.slai.io/ub93k', {
-    method: 'POST',
-    headers: {
-      // authorization value to gain access to beam api end point
-      'Authorization': process.env.AUTHORIZATION,
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-    },
-    body: JSON.stringify({
-      // question to be asked to beam api
-      'query': req.body.query,
-    })
-  });
 
-  const data = await response.json()
-  const {pred} = data
+  const fetchText = async () => {
+    try {
+        const response = await axios.get('https://www.gardendesign.com/seeds/starting.html#:~:text=Plants%20have%20basic%20needs%20of,to%20develop%20into%20healthy%20plants')
 
-  // returns answer information to frontend
-  res.status(200).json(pred)
+        const html = response.data;
 
+        const $ = cheerio.load(html);
+
+        const text = $.text();
+
+        const filteredText = text.match(/\w+/g);
+
+        return filteredText;
+
+    } catch (error) {
+        throw error;
+    }
+  };
+
+  const text = await fetchText();
+
+  res.status(200).json({text: text})
+ 
 }
